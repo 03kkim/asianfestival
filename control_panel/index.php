0313dce6be7c7c8fcd2c57be2fc6d61ee4c8a85f
theq614 <theq614@gmail.com>
2020-08-18 22:03:25 -0400

    Copy paste some stuff into control panel

--- control_panel/index.php
+++ control_panel/index.php
@@ -11,6 +11,51 @@ if(!isset($_GET["action"])){
 }
 
 switch($action) {
+    case "create_practice":
+        if(!$auth->hasRole(\Delight\Auth\Role::SUPER_ADMIN)) {
+            header("Location: /asianfestival/index.php");
+        }
+        $performance_id = filter_input(INPUT_GET, "performance_id");
+        $locations = get_locations();
+        $timeslots = get_timeslots();
+
+        include "view.php";
+        break;
+
+    case "add_practice":
+
+        $submit = filter_input(INPUT_POST, "submit");
+        if ($submit != "cancel") {
+            $performance_id = filter_input(INPUT_POST, "performance_id");
+            $location_id = (filter_input(INPUT_POST, "location"));
+            $date = filter_input(INPUT_POST, "date");
+            $date=date("Y-m-d",strtotime($date));
+            $timeslot = filter_input(INPUT_POST, "timeslot");
+
+            if ($timeslot == "custom") {
+                $start_time = filter_input(INPUT_POST, "start_time");
+                $start_time = date("H:i", strtotime($start_time));
+                $end_time = filter_input(INPUT_POST, "end_time");
+                $end_time = date("H:i", strtotime($end_time));
+
+                create_custom_timeslot($start_time, $end_time);
+
+                // Hopefully, the $start_time and $end_time can be replaced by $time_id once create_custom_timeslot gets fixed
+                create_practice_from_custom_times($performance_id, $location_id, $date, $start_time, $end_time);
+            } else {
+                create_practice($performance_id, $location_id, $date, $timeslot);
+            }
+
+            header("Location: ../control_panel/index.php");
+        }
+        if ($submit == "continue") {
+            header("Location: ../control_panel/index.php");
+        }
+        else {
+            header("Location: ../practices/index.php");
+        }
+
+        break;
     case "show_users":
 
 }
--- control_panel/view.php
+++ control_panel/view.php
@@ -1,1 +1,95 @@
 <?php
+create_header("");
+?>
+
+<div class="container" style="margin-top:5%">
+    <div class="row">
+        <form class="col s12" method="post" action="index.php">
+            <input type="hidden" name="action" value="add_practice">
+            <input type="hidden" name="performance_id" value="<?php echo $performance_id ?>">
+            <div class="row">
+                <div class="input-field col s6">
+                    <select name="location" id="location">
+                        <?php foreach ($locations as $location) { ?>
+                            <option value="<?php echo $location['location_id']?>"><?php echo $location["location_name"]?></option>
+                        <?php } ?>
+                    </select>
+                    <label for="location">Location</label>
+                </div>
+                <div class="input-field col s6">
+                    <select onchange="toggle_custom_time()" name="timeslot" id="timeslot">
+                        <?php foreach ($timeslots as $timeslot) { ?>
+                            // "time" is a concat within an SQL statement in festival_db
+                            <option value="<?php echo $timeslot['time_id']?>"><?php echo $timeslot["time"]?></option>
+                        <?php } ?>
+                        <option value="custom">Custom Time</option>
+                    </select>
+                    <label for="timeslot">Timeslot</label>
+                </div>
+            </div>
+            <div class="row">
+                <div id="start" class="input-field col s6" style="display:none">
+                    <input name="start_time" id="start_time" type="text" class="timepicker">
+                    <label for="start_time">Start Time</label>
+                </div>
+                <div id="end" class="input-field col s6" style="display:none">
+                    <input name="end_time" id="end_time" type="text" class="timepicker">
+                    <label for="end_time">End Time</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-field col s12">
+                    <input name="date" type="text" class="datepicker">
+                    <label for="date">Date</label>
+                </div>
+            </div>
+            <div class="row" style="text-align:center">
+                <div class="input-field col s4">
+                    <button class="btn waves-effect waves-light" value="continue" type="submit" name="submit">Add Another
+                        <i class="material-icons right">send</i>
+                    </button>
+                </div>
+                <div class="input-field col s4">
+                    <button class="btn waves-effect waves-light" value="finish" type="submit" name="submit">Finish
+                        <i class="material-icons right">check</i>
+                    </button>
+                </div>
+                <div class="input-field col s4">
+                    <button class="btn waves-effect waves-light" value="cancel" type="submit" name="submit">Cancel
+                        <i class="material-icons right">cancel</i>
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+<script>
+    $(document).ready(function(){
+        $('select').formSelect();
+        $('.datepicker').datepicker({
+            format: 'yyyy-m-d'
+        });
+        $('.timepicker').timepicker();
+    });
+
+    function toggle_custom_time() {
+        let start = document.getElementById("start");
+        let end = document.getElementById("end");
+
+        let select = document.getElementById("timeslot");
+
+        if(select.options[select.selectedIndex].value == "custom"){
+            start.style.display = "block";
+            end.style.display = "block";
+        }
+        else {
+            start.style.display = "none";
+            end.style.display = "none";
+        }
+    }
+</script>
+
+<?php
+create_footer();
+?>
