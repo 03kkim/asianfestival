963b6909b5111737d0df9aa076967882cd1a1fc3
theq614 <theq614@gmail.com>
2020-08-22 16:28:33 -0400

    Fix last_month()

--- calendar/view.php
+++ calendar/view.php
@@ -8,7 +8,7 @@ create_header("<style>
 ?>
 <h1 style="text-align:center"> Practices for <i style="color:#ff0000;cursor:pointer;font-size:2.5rem" onclick='last_month(<?php echo $month ?>)' class="material-icons">chevron_left</i><?php echo $months[$month-1] ?><i style="color:#ff0000;cursor:pointer;font-size:2.5rem" onclick='next_month(<?php echo $month ?>)' class="material-icons">chevron_right</i> <?php echo $year ?> </h1>
 <script>
-    function next_month(month) {
+    function last_month(month) {
         location.href = "../calendar/index.php?month=" + (month - 1);
     }
     function next_month(month) {
