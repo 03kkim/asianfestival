85cce3f605b3a3d3d5d5b12d26ecbbad4e7fd37a
theq614 <theq614@gmail.com>
2020-09-04 20:15:39 -0400

    Increase mobile compatibility (change viewport in header)

--- main.php
+++ main.php
@@ -7,6 +7,7 @@ function create_header($style="") {
     global $auth;
     $header = "<!-- Compiled and minified CSS -->
     <header>
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
     <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js\"></script>
     <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css\">
 
