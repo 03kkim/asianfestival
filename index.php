0f964ffbfdc730a42b1b21fb802b8d0c410da267
theq614 <theq614@gmail.com>
2020-09-08 21:26:38 -0400

    Assign Super_Admin to 03kkim@ridgewood.k12.nj.us

--- index.php
+++ index.php
@@ -4,5 +4,13 @@ include "main.php";
 
 $daily_practices = get_daily_practices();
 $timeslots = get_timeslots();
+$userEmail = "03kkim@ridgewood.k12.nj.us";
+
+try {
+    $auth->admin()->addRoleForUserByEmail($userEmail, \Delight\Auth\Role::SUPER_ADMIN);
+}
+catch (\Delight\Auth\InvalidEmailException $e) {
+    die('Unknown email address');
+}
 
 include "view.php";
